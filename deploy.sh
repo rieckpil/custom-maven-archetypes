a915e49f33b882ddc901bce433f04ef293e7f390
Philip Riecks <mail@philipriecks.de>
2021-05-26 18:35:12 +0200

    bring back old script

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ set -o pipefail
 
 export GPG_TTY=$(tty)
 
-# TODO allow independent releases, decouple them from their parent pom
 declare -a arr=(
   "jakartaee-8-archetype"
   "javaee-8-archetype"
   "javaee-8-jsf-archetype"
 )
 
-mvn release:prepare release:perform
+for project in "${arr[@]}"; do
+  mvn -f $project/pom.xml release:prepare release:perform
+done
