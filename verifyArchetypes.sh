07b77b27806b5ab49697255aca05c524f2fe6fde
Philip Riecks <mail@philipriecks.de>
2022-08-04 08:18:52 +0200

    adjust verification

--- verifyArchetypes.sh
+++ verifyArchetypes.sh
@@ -14,16 +14,14 @@ cd tmp
 
 for project in "${!archetypes[@]}"; do
 
-  echo "### $project"
-
-  ARCHETYPE_VERSION=$(mvn -q \
+  ARCHETYPE_VERSION=$(../mvnw -q \
     -f "../${archetypes[$project]}/pom.xml" \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
     --non-recursive \
     exec:exec)
 
-  mvn archetype:generate \
+  ../mvnw archetype:generate \
     -DarchetypeGroupId=de.rieckpil.archetypes \
     -DarchetypeArtifactId="$project" \
     -DarchetypeVersion="$ARCHETYPE_VERSION"\
@@ -32,9 +30,12 @@ for project in "${!archetypes[@]}"; do
     -DinteractiveMode=false
 
   cd "$project-test"
+
   ./buildAndRun.sh
+
   timeout 30 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9080/health)" != "200" ]]; do sleep 1; done' || false
   docker stop $(docker ps -a -q)
+
   cd ..
   rm -rf "$project-test"
 done
